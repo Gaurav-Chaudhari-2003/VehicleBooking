15e5af8871cf1a4904d3cfa841c3acb00933d1ea
Gaurav Chaudhari <chaudharigaurav177@gmail.com>
2025-04-28 18:46:48 +0530

    Remove unused admin vendor includes for nav, footer, and sidebar.
    These files are no longer in use and have been deleted to clean up the codebase. This improves maintainability by eliminating redundant or unused code components.

--- admin/vendor/inc/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="sticky-footer">
-        <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span>Copyright &copy; <?php echo date('Y');?> Vehicle Booking System | Designed  By ICT Department, CMPDI R-IV, Nagpur
-           </span>
-          </div>
-        </div>
-</footer>
