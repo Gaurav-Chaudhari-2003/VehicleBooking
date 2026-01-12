01312aef13dd1d1930abc9daaa3534719f889424
Gaurav Chaudhari <chaudharigaurav177@gmail.com>
2026-01-12 13:31:20 +0530

    Fixed driver fired option in the admin-view-driver.php

--- admin/admin-view-driver.php
+++ admin/admin-view-driver.php
@@ -9,16 +9,17 @@ $aid = $_SESSION['a_id'];
 if (isset($_GET['del'])) {
     $id = intval($_GET['del']);
     $adn = "DELETE FROM tms_user WHERE u_id=?";
+    // Ensure $mysqli is available from config.php
+    global $mysqli;
     $stmt = $mysqli->prepare($adn);
     $stmt->bind_param('i', $id);
-    $stmt->execute();
-    $stmt->close();
-
-    if ($stmt) {
+    
+    if ($stmt->execute()) {
         $succ = "Driver Fired";
     } else {
         $err = "Try Again Later";
     }
+    $stmt->close();
 }
 ?>
 
@@ -75,9 +76,7 @@ if (isset($_GET['del'])) {
                                 <th>Contact</th>
                                 <th>Address</th>
                                 <th>Email</th>
-                                <?php if (!isset($_GET['del'])) { ?>
-                                    <th>Action</th>
-                                <?php } ?>
+                                <th>Action</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -95,12 +94,10 @@ if (isset($_GET['del'])) {
                                     <td><?php echo $row->u_phone; ?></td>
                                     <td><?php echo $row->u_addr; ?></td>
                                     <td><?php echo $row->u_email; ?></td>
-                                    <?php if (!isset($_GET['del'])) { ?>
-                                        <td>
-                                            <a href="admin-manage-single-driver.php?u_id=<?php echo $row->u_id;?>" class="badge badge-success">Update</a>
-                                            <a href="admin-manage-driver.php?del=<?php echo $row->u_id;?>" class="badge badge-danger">Fire</a>
-                                        </td>
-                                    <?php } ?>
+                                    <td>
+                                        <a href="admin-manage-single-driver.php?u_id=<?php echo $row->u_id;?>" class="badge badge-success">Update</a>
+                                        <a href="admin-view-driver.php?del=<?php echo $row->u_id;?>" class="badge badge-danger" onclick="return confirm('Are you sure you want to fire this driver?');">Fire</a>
+                                    </td>
                                 </tr>
                                 <?php $cnt = $cnt + 1; } ?>
                             </tbody>
@@ -116,30 +113,6 @@ if (isset($_GET['del'])) {
 </div>
 <!-- /#wrapper -->
 
-<!-- Scroll to Top Button-->
-<a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-</a>
-
-<!-- Logout Modal-->
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-danger" href="admin-logout.php">Logout</a>
-            </div>
-        </div>
-    </div>
-</div>
-
 <!-- Bootstrap core JavaScript-->
 <script src="vendor/jquery/jquery.min.js"></script>
 <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -152,10 +125,13 @@ if (isset($_GET['del'])) {
 <script src="vendor/datatables/dataTables.bootstrap4.js"></script>
 
 <!-- Custom scripts for all pages-->
-<script src="js/sb-admin.min.js"></script>
+<script src="vendor/js/sb-admin.min.js"></script>
 
 <!-- Demo scripts for this page-->
-<script src="js/demo/datatables-demo.js"></script>
+<script src="vendor/js/demo/datatables-demo.js"></script>
+
+<!-- SweetAlert -->
+<script src="vendor/js/swal.js"></script>
 
 <script>
     $(document).ready(function() {
